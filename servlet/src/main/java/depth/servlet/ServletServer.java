cb6175797790fe0b4efc79dfd49b19850505323f
이지수 <0801ljs@naver.com>
2025-05-02 00:46:34 +0900

    [REFACTOR] 요청 처리에 ThreadPoolExecutor 적용

--- servlet/src/main/java/depth/servlet/ServletServer.java
+++ servlet/src/main/java/depth/servlet/ServletServer.java
@@ -5,10 +5,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServletServer {
     private final Map<String, MyServlet> servletMap = new HashMap<>();
 
+    //스레드풀 필드 추가
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(10);
     public void registerServlet(String path, MyServlet servlet) {
         servletMap.put(path, servlet); // 경로 등록
     }
@@ -18,7 +22,8 @@ public class ServletServer {
         System.out.println("Server started at port " + port);
         while (true) {
             Socket socket = serverSocket.accept();
-            new Thread(() -> handle(socket)).start(); // 요청마다 새 스레드
+            //new Thread(() -> handle(socket)).start(); // 요청마다 새 스레드
+            threadPool.execute(() -> handle(socket)); // 새 스레드 대신 threadPool 사용
         }
     }
 
@@ -29,6 +34,8 @@ public class ServletServer {
         ) {
             HttpRequest request = new HttpRequest(in);     // 요청 파싱
             HttpResponse response = new HttpResponse(out); // 응답 생성
+
+            System.out.println("Processing by: " + Thread.currentThread().getName());
 
             MyServlet servlet = servletMap.getOrDefault(request.getPath(), new NotFoundServlet());
             servlet.service(request, response); // 서블릿 위임
